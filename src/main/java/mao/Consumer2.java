94bc182a087459b383d65440e38609d957535b18
毛 <1296193245@qq.com>
2022-04-23 13:12:30 +0800

    rabbitMQ_exchange_topic_exchange

---
+++ src/main/java/mao/Consumer2.java
@@ -0,0 +1,52 @@
+package mao;
+
+import com.rabbitmq.client.*;
+import mao.tools.RabbitMQ;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Project name(项目名称)：rabbitMQ交换机之topic交换机
+ * Package(包名): mao
+ * Class(类名): Consumer2
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/4/23
+ * Time(创建时间)： 12:31
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class Consumer2
+{
+    private static final String EXCHANGE_NAME = "topic_exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException
+    {
+        Channel channel = RabbitMQ.getChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        String queue = channel.queueDeclare().getQueue();
+        channel.queueBind(queue, EXCHANGE_NAME, "*.true.*.*");
+        channel.basicConsume(queue, false, new DeliverCallback()
+        {
+            @Override
+            public void handle(String consumerTag, Delivery message) throws IOException
+            {
+                byte[] messageBody = message.getBody();
+                String message1 = new String(messageBody, StandardCharsets.UTF_8);
+                System.out.println("消费者2接收到消息：" + message1);
+                channel.basicNack(message.getEnvelope().getDeliveryTag(), false, false);
+            }
+        }, new CancelCallback()
+        {
+            @Override
+            public void handle(String consumerTag) throws IOException
+            {
+                System.out.println("Cancel");
+            }
+        });
+    }
+}
---
+++ src/main/java/mao/Producer.java
@@ -0,0 +1,43 @@
+package mao;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import mao.tools.RabbitMQ;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Project name(项目名称)：rabbitMQ交换机之topic交换机
+ * Package(包名): mao
+ * Class(类名): Producer
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/4/23
+ * Time(创建时间)： 12:31
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class Producer
+{
+    private static final String EXCHANGE_NAME = "topic_exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException
+    {
+        Channel channel = RabbitMQ.getChannel();
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+
+        while (true)
+        {
+            channel.basicPublish(EXCHANGE_NAME,"false.true.true.false",null,"2-3收到".getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGE_NAME,"true.true.true.false",null,"1-3收到".getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGE_NAME,"false.true.false.true",null,"2,4收到".getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGE_NAME,"false.true.true.true",null,"2-4收到".getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish(EXCHANGE_NAME,"true.true.true.true",null,"1-4收到".getBytes(StandardCharsets.UTF_8));
+        }
+
+    }
+}
